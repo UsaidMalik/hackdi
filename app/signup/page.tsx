cfc3732e0911f87faa52b54c2e6a091c48b3c13a
reyhanquayum <reyhan.quayum@gmail.com>
2025-06-28 15:16:31 -0400

    feat: signup link on login page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,26 +3,30 @@ import { redirect } from "next/navigation"
 import SignUpForm from "../_components/signupForm"
 import Link from 'next/link'
 
-const SignUp = async () => {  
-  const session = await getSession()
+const SignUp = async () => {
+  const session = await getSession();
 
-  if(session.isLoggedIn){
-    redirect("/")
+  if (session.isLoggedIn) {
+    redirect('/');
   }
-  
+
   return (
     <div className="p-4">
-      <h1 className="flex justify-center text-4xl my-10">Ready To Transfer?</h1>
-      <div className="flex justify-center items-center flex-col">
-      <SignUpForm/>
-      <h1 className="text-xs mt-5 text-gray-500">Already A User? {"  "}
-      <Link href={"/login"} className="text-decoration-line underline text-blue-400 hover:text-gray-600">
-         Login Instead
-      </Link>
-    </h1>
+      <h1 className="my-10 flex justify-center text-4xl">Muqata'ah</h1>
+      <div className="flex flex-col items-center justify-center">
+        <SignUpForm />
+        <h1 className="mt-5 text-xs text-gray-500">
+          Already A User?{' '}
+          <Link
+            href={'/login'}
+            className="text-decoration-line text-blue-400 underline hover:text-gray-600"
+          >
+            Login Instead
+          </Link>
+        </h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignUp
