a655d6c1fe03bea054ca073debb7e5b0ba57a50c
UsaidMalik2 <umalik2003@gmail.com>
2025-06-29 07:35:54 -0400

    uncommited

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Welcome to MJM3",
   description: "Aggregate everything",
+   icons: {
+    icon: "/favicon.jpg",
+  },
 };
 
 export default function RootLayout({
