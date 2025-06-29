8b1fcea9e70044c7a97dee0c09c3a4c9e00a4155
reyhanquayum <reyhan.quayum@gmail.com>
2025-06-29 00:22:15 -0400

    ui fixes, navbar, logout visual changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import SearchBar from "@/app/_components/searchBar";
-import QueryEntityMapper from "@/app/_components/QueryEntityMapper";
-import LogoutButton from "@/app/_components/LogoutButton";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-gray-900 px-4">
-      <LogoutButton />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-gray-900 px-4 -mt-16">
       <h1 className="text-4xl md:text-5xl font-bold mb-8 text-center tracking-tight">
         Mqt3
       </h1>
