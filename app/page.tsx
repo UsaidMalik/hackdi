b1dbb89a54a44decfa6384cc47e96138be33ef0b
UsaidMalik2 <umalik2003@gmail.com>
2025-06-29 05:37:41 -0400

    UI Enhancements

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,7 @@ import ProfileComponent from "@/app/_components/ProfileComponent";
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-white text-gray-900 px-4">
-      {/* Top bar: Profile + Logout aligned on top */}
-      <div className="w-full max-w-6xl flex justify-between items-center py-4 mx-auto">
-        <ProfileComponent />
-        <LogoutButton />
-      </div>
+      
 
       {/* Centered content below header */}
       <div className="flex flex-col items-center justify-center flex-grow">
